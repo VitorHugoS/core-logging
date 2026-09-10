7734a1693059af10086adb80d77f7f658c96779e
Vitor Hugo <vitorhugoss17@gmail.com>
2026-09-10 13:34:38 -0300

    chore: clean up patch scripts

--- patch_autoconfig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i '' -e 's/public CoreLoggerFactory coreLoggerFactory(tools.jackson.databind.ObjectMapper objectMapper) {/public CoreLoggerFactory coreLoggerFactory(tools.jackson.databind.ObjectMapper objectMapper, CoreLoggingProperties properties) {/g' src/main/java/com/corelogging/config/CoreLoggingAutoConfiguration.java
-sed -i '' -e 's/return new CoreLoggerFactory(objectMapper);/return new CoreLoggerFactory(objectMapper, properties);/g' src/main/java/com/corelogging/config/CoreLoggingAutoConfiguration.java
